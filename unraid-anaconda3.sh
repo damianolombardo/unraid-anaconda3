67be5415e68bbc12f06f0e39294fff748691c498
Damiano Lombardo <lombardo.damiano@gmail.com>
2018-04-29 21:22:51 +0800

    Changed the template to try and clarify what is being executed on run. Added a startup script which sets a whole bunch of variables based on the unraid input It also cleans up the image and sets an external conda cache.

---
+++ unraid-anaconda3.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -v
+
+$CONDA_EXE clean --all -y  
+$CONDA_EXE config --prepend pkgs_dirs /opt/conda_cache
+
+$CONDA_EXE update -n base conda -y 
+$CONDA_EXE install jupyter -y 
+$CONDA_EXE install -c conda-forge jupyter_contrib_nbextensions -y 
+
+$CONDA_EXE install $CONDAPACKAGES -y 
+
+export NOTEBOOKPASSWORDHASHED=$($CONDA_PYTHON_EXE -c "from notebook.auth import passwd
+from os import environ
+passwd(environ['NOTEBOOKPASSWORD'])")
+
+/opt/conda/bin/jupyter notebook --notebook-dir=\'$JUPYTER_SERVER_ROOT\' --ip=\'$NOTEBOOKIP\' --port=$NOTEBOOKPORT --no-browser --allow-root --NotebookApp.password=\'$NOTEBOOKPASSWORDHASHED\'
+
+
