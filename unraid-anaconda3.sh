67129b23c2466383a426d6b76df995477755dd64
Damiano Lombardo <lombardo.damiano@gmail.com>
2018-04-29 22:23:43 +0800

    change cache directory name, added export of CONDA_PYTHON_EXE

--- unraid-anaconda3.sh
+++ unraid-anaconda3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export CONDA_EXE='/opt/conda/bin/conda'
+export CONDA_PYTHON_EXE='opt/conda/bin/python'
+
+$CONDA_EXE config --remove pkgs_dirs /opt/conda_pkgs_cache
+cp -n /opt/conda/pkgs/* /opt/conda_pkgs_cache/
 $CONDA_EXE clean --all -y  
-$CONDA_EXE config --prepend pkgs_dirs /opt/conda_cache
+$CONDA_EXE config --prepend pkgs_dirs /opt/conda_pkgs_cache
 
 $CONDA_EXE update -n base conda -y 
 $CONDA_EXE install jupyter -y 
