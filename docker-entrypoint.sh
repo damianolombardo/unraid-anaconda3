e61c94c3a8a9ab2be0553085adc0d010f26767e1
Damiano Lombardo <lombardo.damiano@gmail.com>
2019-03-26 22:47:47 +0800

    trying some smarter ways to get this working quickly

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
-export CONDA_EXE='/opt/conda/bin/conda'
-export CONDA_PYTHON_EXE='opt/conda/bin/python'
+#path to conda binary
+export CONDA_BIN='/opt/conda/bin/conda'
+#path to conda python binary
+export CONDA_PYTHON_BIN='opt/conda/bin/python'
 
 echo setting conda cache
-$CONDA_EXE config --remove pkgs_dirs /opt/conda_pkgs_cache
+#temporarily remove the external conda package cache
+$CONDA_BIN config --remove pkgs_dirs /opt/conda_pkgs_cache
+#copy any cached packages to the external conda package cache
 cp -n -r /opt/conda/pkgs/* /opt/conda_pkgs_cache/
-$CONDA_EXE clean --all -y  
-$CONDA_EXE config --prepend pkgs_dirs /opt/conda_pkgs_cache
+#empty internal conda package cache
+$CONDA_BIN clean --all -y  
+#re add the external conda package cache
+$CONDA_BIN config --prepend pkgs_dirs /opt/conda_pkgs_cache
 
 echo update conda cache
-$CONDA_EXE update -n base conda -y 
-$CONDA_EXE install jupyter -y 
-$CONDA_EXE install -c conda-forge jupyter_contrib_nbextensions -y 
+#set where conda will install these packages
+export CONDA_INSTALL_PATH='/opt/conda/dist-packages'
+#update conda
+$CONDA_BIN update -n base conda -y 
+#install jupyter
+$CONDA_BIN install -p $CONDA_INSTALL_PATH jupyter -y 
+#install notebook extensions
+$CONDA_BIN install -p $CONDA_INSTALL_PATH -c conda-forge jupyter_contrib_nbextensions -y 
 echo install conda packages
-$CONDA_EXE install $CONDA_PACKAGES -y 
+#install custom packages
+$CONDA_BIN install -p $CONDA_INSTALL_PATH $CONDA_PACKAGES -y 
 
 #pip --cache-dir /opt/pip_pkgs_cache
 echo install pip packages
-pip install $PIP_PACKAGES
+
+#set the pip install path
+export PIP_INSTALL_PATH='/opt/pip/dist-packages'
+export PYTHONPATH=$PIP_INSTALL_PATH:$PYTHONPATH
+pip install --target=$PIP_INSTALL_PATH $PIP_PACKAGES
 
 
 echo "from notebook.auth import passwd
 from os import environ
 print(passwd(environ['NOTEBOOK_PASSWORD']))" > pwgen.py
 
-export NOTEBOOK_PASSWORD_HASHED=$($CONDA_PYTHON_EXE pwgen.py)
+export NOTEBOOK_PASSWORD_HASHED=$($CONDA_PYTHON_BIN pwgen.py)
 
 if [ ! -f /root/.jupyter/jupyter_notebook_config.py ]; then
     /opt/conda/bin/jupyter notebook --generate-config
